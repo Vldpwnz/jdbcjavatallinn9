c608f4499aa256886a0f5e53e77c5b1c435332a0
Vlad <vladislavcherkashin98@gmail.com>
2020-02-29 11:55:36 +0200

    Solving duplicate problem

--- src/main/java/JDBCpackage/Prepeare.java
+++ src/main/java/JDBCpackage/Prepeare.java
@@ -10,12 +10,16 @@ public class Prepeare {
     public void prepearedStatement(String name, String address, String phone, int age) throws SQLException{
         Connection connector = DriverManager.getConnection(url, user, pass);
         Statement statement = connector.prepareStatement("SELECT id, name, address, phone, FROM driver WHERE name = ????");
+
         String INSERT = "INSERT INTO driver(name, address, phone, age) VALUES (?, ?, ?, ?);";
+
         PreparedStatement prstm = connector.prepareStatement(INSERT);
+
         prstm.setString(1,name);
         prstm.setString(2, address);
         prstm.setString(3,phone);
         prstm.setInt(4,age);
+
         int ret = prstm.executeUpdate();
 
 
